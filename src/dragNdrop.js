d43ac06ab8d1ebbcd01814fe1d2e53801093fb07
Li Zheng <flyskywhy@gmail.com>
2022-12-21 19:59:29 +0800

    babylonjs Drag and drop: demo the ability of multi View

--- src/dragNdrop.js
+++ src/dragNdrop.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { Engine, Scene } from 'react-native-babylonjs';
 import { Vector3, Color3 } from '@babylonjs/core';
 
@@ -11,7 +17,8 @@ const validateDrag = (targetPosition) => {
 
 function dragNdrop() {
   return (
-    <View style={{flex: 1}}>
+    <View style={styles.container}>
+      <View style={styles.container}>
           <Engine antialias={true} engineOptions={{preserveDrawingBuffer:true, stencil:true}} canvasId="sample-canvas">
             <Scene clearColor={new Color3(0, 0, 0)}>
             <pointLight name='omni' position={new Vector3(0, 50, 0)} />
@@ -44,8 +51,70 @@ function dragNdrop() {
             </torus>
             </Scene>
           </Engine>
+      </View>
+
+      <TouchableOpacity
+        accessibilityRole={'button'}
+        onPress={() =>
+          Linking.openURL(
+            'https://brianzinn.github.io/create-react-app-babylonjs/dragNdrop',
+          )
+        }
+        style={styles.linkContainer}>
+        <Text style={styles.link}>Click me to contrast the Web version</Text>
+      </TouchableOpacity>
+
+      <View style={styles.container}>
+          <Engine antialias={true} engineOptions={{preserveDrawingBuffer:true, stencil:true}} canvasId="sample-canvas">
+            <Scene clearColor={new Color3(0, 0, 0)}>
+            <pointLight name='omni' position={new Vector3(0, 50, 0)} />
+            <arcRotateCamera name='camera' alpha={0} beta={0} radius={10} target={Vector3.Zero()} setPosition={[new Vector3(20, 200, 400)]}
+              lowerBetaLimit={0.1} upperBetaLimit={(Math.PI / 2) * 0.99} lowerRadiusLimit={150}
+            />
+
+            <ground name='ground' width={GROUND_SIZE} height={GROUND_SIZE} subdivisions={1}>
+              <standardMaterial name='groundMat' specularColor={Color3.Black()} />
+            </ground>
+
+            <sphere name='red' diameter={20} segments={32} position={new Vector3(-100, 10, 0)}>
+              <standardMaterial name='redMat' diffuseColor={new Color3(0.4, 0.4, 0.4)} specularColor={new Color3(0.4, 0.4, 0.4)} emissiveColor={Color3.Red()} />
+              <pointerDragBehavior dragPlaneNormal={new Vector3(0,1,0)} validateDrag={validateDrag} />
+            </sphere>
+
+            <box name='green' size={20} position={new Vector3(0, 11, -100)}>
+              <standardMaterial name='greenMat' diffuseColor={new Color3(0.4, 0.4, 0.4)} specularColor={new Color3(0.4, 0.4, 0.4)} emissiveColor={Color3.Green()} />
+              <pointerDragBehavior dragPlaneNormal={new Vector3(0,1,0)} validateDrag={validateDrag} />
+            </box>
+
+            <box name='blue' size={20} position={new Vector3(100, 11, 0)}>
+              <standardMaterial name='greenMat' diffuseColor={new Color3(0.4, 0.4, 0.4)} specularColor={new Color3(0.4, 0.4, 0.4)} emissiveColor={Color3.Blue()} />
+              <pointerDragBehavior dragPlaneNormal={new Vector3(0,1,0)} validateDrag={validateDrag} />
+            </box>
+
+            <torus name='torus' diameter={30} thickness={10} tesselation={32} position={new Vector3(0, 10, 100)}>
+              <standardMaterial name='torusMat' diffuseColor={new Color3(0.4, 0.4, 0.4)} specularColor={new Color3(0.4, 0.4, 0.4)} emissiveColor={Color3.Purple()} />
+              <pointerDragBehavior dragPlaneNormal={new Vector3(0,1,0)} validateDrag={validateDrag} />
+            </torus>
+            </Scene>
+          </Engine>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  linkContainer: {
+    alignSelf: 'center',
+    paddingVertical: 15,
+  },
+  link: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: 'blue',
+  },
+});
 
 export default dragNdrop
