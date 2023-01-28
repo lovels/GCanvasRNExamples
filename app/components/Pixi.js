21baa7100af91cf79d45706ab764dd930209022d
Li Zheng <flyskywhy@gmail.com>
2023-01-28 09:03:45 +0800

    PixiJS: support Web; convert function component to class component

--- app/components/Pixi.js
+++ app/components/Pixi.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
 import {GCanvasView} from '@flyskywhy/react-native-gcanvas';
 import {PIXI} from 'react-native-pixi';
 import {Asset} from 'expo-asset';
@@ -6,11 +7,57 @@ import {Asset} from 'expo-asset';
 // for game, 1 is more better than PixelRatio.get() to code with physical pixels
 const devicePixelRatio = 1;
 
-export default () => {
-  const onCanvasCreate = async (canvas) => {
-    let context = canvas.getContext('webgl');
+export default class Pixi extends Component {
+  constructor(props) {
+    super(props);
+    this.canvas = null;
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'web') {
+      const resizeObserver = new ResizeObserver((entries) => {
+        for (let entry of entries) {
+          if (entry.target.id === 'canvasExample') {
+            let {width, height} = entry.contentRect;
+            this.onCanvasResize({width, height, canvas: entry.target});
+          }
+        }
+      });
+      resizeObserver.observe(document.getElementById('canvasExample'));
+    }
+  }
+
+  initCanvas = (canvas) => {
+    if (this.canvas) {
+      return;
+    }
+
+    this.canvas = canvas;
+    if (Platform.OS === 'web') {
+      // canvas.width not equal canvas.clientWidth but "Defaults to 300" ref
+      // to https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas,
+      // so have to assign again, unless <canvas width=SOME_NUMBER/> in render()
+      this.canvas.width = this.canvas.clientWidth;
+      this.canvas.height = this.canvas.clientHeight;
+    }
+    // should not name this.context because this.context is already be {} here and will
+    // be {} again after componentDidUpdate() on react-native or react-native-web, so
+    // name this.ctx
+    this.ctx = this.canvas.getContext('webgl');
+
+    this.drawSome();
+  };
+
+  onCanvasResize = ({width, height, canvas}) => {
+    // TODO: debug resize in pixi.js
+    // canvas.width = width;
+    // canvas.height = height;
+    // this.drawSome();
+  };
+
+  drawSome = async () => {
     const app = new PIXI.Application({
-      context,
+      context: this.ctx,
       devicePixelRatio,
       backgroundColor: '0x7ed321',
     });
@@ -105,11 +152,43 @@ export default () => {
     }
   };
 
-  return (
-    <GCanvasView
-      style={{flex: 1, height: '100%'}}
-      onCanvasCreate={onCanvasCreate}
-      devicePixelRatio={devicePixelRatio}
-    />
-  );
-};
+  render() {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'web' ? (
+          <canvas
+            id={'canvasExample'}
+            ref={this.initCanvas}
+            style={
+              {
+                flex: 1,
+                width: '100%',
+              } /* canvas with react-native-web can't use width and height in styles.gcanvas */
+            }
+          />
+        ) : (
+          <GCanvasView
+            style={styles.gcanvas}
+            onCanvasResize={this.onCanvasResize}
+            onCanvasCreate={this.initCanvas}
+            devicePixelRatio={devicePixelRatio}
+          />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  gcanvas: {
+    flex: 1,
+    width: '100%',
+    // backgroundColor: '#FF000030', // TextureView doesn't support displaying a background drawable since Android API 24
+  },
+});
