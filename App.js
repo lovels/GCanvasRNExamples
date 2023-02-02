345075be05de8bee39d2303f79ff9c9bc04c1790
Li Zheng <flyskywhy@gmail.com>
2023-02-02 17:32:58 +0800

    RN 0.63.2 -> 0.70.5: cherry-pick from branch RnUpgrade

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import {Button, Platform, StyleSheet, View, StatusBar} from 'react-native';
+import {Button, Platform, View, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Canvas2dDemoScreen from './app/components/Canvas2dDemo';
@@ -61,7 +61,7 @@ function HomeScreen({navigation}) {
 
 const Stack = createNativeStackNavigator();
 
-const App: () => React$Node = () => {
+const App: () => Node = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
@@ -110,39 +110,5 @@ const App: () => React$Node = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: 'grey',
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: 'white',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: 'black',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: 'black',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
